955cc323f4678ed825c26dae71b68207640fbf12
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-05 19:21:48 +0100

    Modifies main component css for better integration in single-spa root application

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="app">
-    <h1><img src="https://vuejs.org/images/logo.png" width="50" height="50"> Vue App</h1>
-    <hr>
-    <p>This is the Vue section page. This section is displayed when url starts with "/vue" prefix.
-    </p>
-    <p id="links">
-        <router-link to="/">List</router-link>
-        <router-link to="/detail">Detail</router-link>
-    </p>
-    <router-view></router-view>
+  <div id="vue-app">
+      <div id="vue-component">
+          <div class="main">
+            <h1><img src="https://vuejs.org/images/logo.png" width="50" height="50"> Vue App</h1>
+            <hr>
+            <p>
+                This is the Vue section page.
+                This section is displayed when url starts with "/vue" prefix.
+            </p>
+            <p id="links">
+                <router-link to="/">List</router-link>
+                <router-link to="/detail">Detail</router-link>
+            </p>
+            <router-view></router-view>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -20,20 +26,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#app {
-  padding: 0 15px 0;
-  #links{
-    margin: 30px 0 30px;
-    a{
-      padding: 10px;
-      display: inline-block;
-      width: 75px;
-      text-align: center;
+#vue-component {
+  display: block;
+  padding: 77px 0 77px 150px;
 
-      &.router-link-exact-active{
-        background-color: #007bff;
-        color: #fff;
-        border-radius: 4px;
+  .main{
+    padding: 0 15px;
+
+    #links{
+      margin: 30px 0 30px;
+      a{
+        padding: 10px;
+        display: inline-block;
+        width: 75px;
+        text-align: center;
+
+        &.router-link-exact-active{
+          background-color: #007bff;
+          color: #fff;
+          border-radius: 4px;
+        }
       }
     }
   }
