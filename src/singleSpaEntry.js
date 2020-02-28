755341fc42ff8a06fc576fdc136ca3bbc2fff24b
Juan Manuel López Pazos <juanmalp1992@gmail.com>
2020-02-28 19:14:11 +0100

    remove bootstrap css import

--- src/singleSpaEntry.js
+++ src/singleSpaEntry.js
@@ -5,9 +5,6 @@ import { BootstrapVue } from 'bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-
 Vue.use(BootstrapVue);
 
 Vue.use(VueToastr, {
